9b79452276cadb20a8b21c7935dec14c437386d2
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2023-09-13 17:38:33 -0400

    Initial commit

---
+++ src/test/java/dev/dhruvin/kamkaj/KamKajApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dhruvin.kamkaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamKajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
