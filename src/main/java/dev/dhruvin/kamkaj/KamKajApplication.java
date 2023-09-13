1bff2a041c4aedb7bb91c046d6bc63b62a4e665c
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2023-09-13 17:44:12 -0400

    Initial commit file

---
+++ src/main/java/dev/dhruvin/kamkaj/KamKajApplication.java
@@ -0,0 +1,13 @@
+package dev.dhruvin.kamkaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamKajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamKajApplication.class, args);
+	}
+
+}
